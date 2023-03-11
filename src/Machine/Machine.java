278f28b07e6856f84b2c6553445fe24d7b41f013
Vyom Verma <vyomverma2003@gmail.com>
2023-03-12 02:13:41 +0530

    Modified Client

--- src/Machine/Machine.java
+++ src/Machine/Machine.java
@@ -8,11 +8,15 @@ public class Machine {
     //private InetAddress selfAddress;
     String ac_address;
     int ac_port;
+    Queue<Packet> buffer;
+    Queue<Packet> receiveBuffer;
 
     public Machine(String ac_address, int ac_port) throws IOException{
         //selfAddress  = Binder.getAddress();
         this.ac_address = ac_address;
         this.ac_port = ac_port;
+        this.buffer = new LinkedList<Packet>();
+        this.receiveBuffer = new LinkedList<Packet>();
     }
 
     public void initiate() throws IOException{
@@ -25,7 +29,7 @@ public class Machine {
             ObjectOutputStream oos = new ObjectOutputStream(s.getOutputStream());
             System.out.println("Connected to server ... ");
             
-            
+            /*
             Packet p = new Packet(0);
             p.msg_name = "GO FOR THE HEAD!!";
             System.out.printf("IP AC: ");
@@ -37,9 +41,9 @@ public class Machine {
             else {
             	System.out.println("Listening for data!");
             }
+            */
             
-            
-            Runnable recv = new Runnable() {
+            Runnable receivePackets = new Runnable() {
             	@Override
             	public void run() {
             		Packet p;
@@ -47,23 +51,154 @@ public class Machine {
             		while (true) {
             			try {
     						p = (Packet) ois.readObject();
-    						//if (p.destination_ip == InetAddress.getLocalHost().getHostAddress());
-    						System.out.println(p.msg_name);
+    						if (p.payload.length == 0) {
+    							break;
+    						}
+    						//System.out.println(p.msg_name);
+    						receiveBuffer.add(p);
     						
     					} catch (ClassNotFoundException e) {
-    						// TODO Auto-generated catch block
-    						//e.printStackTrace();
+
     					} catch (IOException e) {
-    						// TODO Auto-generated catch block
-    						//e.printStackTrace();
+
     					}
             		}
+            		System.out.println("Creating File!!");
+            		Vector<Byte> file = new Vector<Byte>();
+            		//final String outputPath = receiveBuffer.peek().msg_name; 
+            		final String outputPath = "newOutFile.txt";
+                    while (!receiveBuffer.isEmpty()){
+                    	Packet pkt = receiveBuffer.poll();
+                    	
+                        for (int j=0; j<pkt.payload.length && pkt.payload[j] != (byte)'\0'; j++){
+                            file.add((Byte) pkt.payload[j]);
+                        }
+                    }
+                    
+                    byte[] byteFile = new byte[file.size()];
+                    for (int i=0; i<file.size(); i++) {
+                    	byteFile[i] = (byte) file.elementAt(i);
+                    }
+                    
+                    try {
+	            		FileOutputStream fos = new FileOutputStream(outputPath);
+	                    fos.write(byteFile);
+	                    fos.close();
+                    }
+                    catch(IOException e) {
+                    	System.out.println("Error Saving File!");
+                    }
 					
             	}
             };
             
-            Thread t = new Thread(recv);
-            t.start();
+            
+            
+            System.out.println("Recieve file(y) or send file(n)?");
+            String ans = sc.next();
+            if (ans == "y") {
+            	Thread receivePacketsThread = new Thread(receivePackets);
+                receivePacketsThread.start();
+            }
+            else {
+            	String path, destIP, clientIP = InetAddress.getByName("localhost").getHostAddress();
+                
+                System.out.printf("Name file to send: ");
+                path = sc.next();
+                
+                System.out.printf("IP of Destination: ");
+                destIP = sc.next();
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)); 
+
+                byte[] file;
+                File fileobj = new File(path);
+
+                try {
+                    FileInputStream fis = new FileInputStream(fileobj);
+                    file = new byte[(int) fileobj.length()];
+                    fis.read(file);
+                    fis.close();
+                }
+                catch(IOException e) {
+                    System.out.print("Exception while opening file: ");
+                    e.printStackTrace();
+                    return;
+                }
+                
+                int pyld_size = 7;
+                final int pkt_total = file.length/pyld_size + (file.length%pyld_size == 0 ? 0 : 1);
+                //if (file.length%pyld_size != 0) pkt_total++;
+                System.out.printf("\nMessage Size: %d Payload Size: %d Total Packets: %d\n\n",file.length, pyld_size, pkt_total);
+                
+                
+                
+                Runnable fragmentPackets = new Runnable() {
+                	@Override
+                	public void run() {
+                		int index = 0;
+                        //Packet[] pkts = new Packet[pkt_total];
+                        for (int i=0; i<pkt_total; i++){
+                            Packet pkt= new Packet(pyld_size);
+                            pkt.client_name = "localhost";
+                            pkt.client_ip = clientIP;
+                            pkt.destination_ip = destIP;
+                            pkt.pkt_no = i;
+                            pkt.pkt_id = i; 
+                            pkt.msg_name = path;
+                            int j=0;
+                            for (; j<pyld_size && index<file.length; j++){
+                                pkt.payload[j] = (byte) file[index];
+                                index++;
+                            }
+                            if (index==file.length && j<pyld_size){
+                                pkt.payload[j] = (byte) '\0';
+                            }
+                            synchronized(buffer) {
+                            	buffer.add(pkt);
+                            }
+                        }
+                        Packet end = new Packet(0);
+                        try {
+                        	oos.writeObject(end);
+                        }
+                        catch(IOException e) {
+                        	
+                        }
+     
+                	}
+                };
+                
+                Thread fragmentPacketsThread = new Thread(fragmentPackets);
+                fragmentPacketsThread.start();
+                
+                Runnable sendPackets = new Runnable() {
+                	@Override
+                	public void run() {
+                		while (true) {
+    	            		synchronized(buffer) {
+    	            			if (!buffer.isEmpty()) {
+    	            				try {
+    	            					oos.writeObject(buffer.peek());
+    	            				}
+    	            				catch (IOException e) {
+    	            					
+    	            				}
+    	            			}
+    	            		}
+                		}
+                		
+                	}
+                };
+                
+                Thread sendPacketsThread = new Thread(sendPackets);
+                sendPacketsThread.start();
+                 
+                Thread receivePacketsThread = new Thread(receivePackets);
+                receivePacketsThread.start();
+             
+            }
+            
               
             //ois.close();
             //oos.close();
--- src/Message/Packet.java
+++ src/Message/Packet.java
@@ -2,15 +2,15 @@ package Message;
 
 import java.io.Serializable;
 
-public class Packet implements Serializable{
-	private static final long serialVersionUID = 1L;
-	public String client_name;
-	public String client_ip;
-	public String destination_ip;
-    byte[] payload;
-    int pkt_no;
+public class Packet implements Serializable {
+    public String client_name;
+    public String client_ip;
+    public String destination_ip;
+    public byte[] payload;
+    public int pkt_no;
+    public int pkt_id;
     public String msg_name;
     public Packet(int payload_size){
         payload = new byte[payload_size];
     }
-}
+};
